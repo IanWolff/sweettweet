da53c6db521ebd36d479642068421f0ac126be2a
Paxton Head <phead@email.wm.edu>
2017-04-01 18:09:44 -0400

    everything gone but index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
-import Inputfield from '../components/inputfield'
-
 export default class Index extends React.Component{
-  render() {
-    return (
-      <h1>
-        Sweet Tweet
-        <Inputfield/>
-      </h1>
+  render(){
+    return(
+      <html>
+        <body>
+          <h1>
+            Sweet Tweet
+            </h1>
+            <form>
+            Twitter Handle:<br/>
+            <input type="text" name ="handle"/><br/>
+            </form>
+          </body>
+        </html>
     )
+
   }
+
 }

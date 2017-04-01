14d93186c821801609bc842b8aef9ca7e26900ac
Paxton Head <phead@email.wm.edu>
2017-04-01 16:44:32 -0400

    Add components

---
+++ components/inputfield.js
@@ -0,0 +1,10 @@
+export default class Inputfield extends React.Component{
+  render() {
+    return(
+      <form>
+        Twitter Handle:<br/>
+        <input type="text" name ="handle"/><br/>
+      </form>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import Inputfield from '../components/inputfield'
+
 export default class Index extends React.Component{
   render() {
     return (
       <h1>
         Sweet Tweet
+        <Inputfield/>
       </h1>
     )
   }
